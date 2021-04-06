b3b74006e4d28b596fc5b1e40d505792c42e46dd
Marina8989 <marina.mill209@gmail.com>
2021-04-06 00:00:50 -0700

    working on js part of the project

---
+++ main.js
@@ -0,0 +1,8 @@
+let userCount = 0;
+let compCount = 0;
+let userScore = document.querySelector('#userScore');
+let compScore = document.querySelector('#compScore');
+let messageHeader = document.querySelector('.message > h2');
+const r = document.querySelector('.r');
+const p = document.querySelector('.p');
+const s = document.querySelector('.s');
