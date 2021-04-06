08a6f781abb8f6fd612c30796ad978c0b1b60ec2
Marina8989 <marina.mill209@gmail.com>
2021-04-06 00:13:25 -0700

    working on js part of the project

--- main.js
+++ main.js
@@ -6,3 +6,31 @@ let messageHeader = document.querySelector('.message > h2');
 const r = document.querySelector('.r');
 const p = document.querySelector('.p');
 const s = document.querySelector('.s');
+
+
+function findOption() {
+    let option = ['r', 'p', 's']
+    let randOption = Math.floor(Math.random() * option.length);
+    console.log(randOption);
+}
+
+
+function game(choice) {
+    schoice = findOption();
+}
+
+
+function makeGame() {
+
+    r.addEventListener('click', function() {
+        game('r');
+    })
+    p.addEventListener('click', function() {
+       game('p');
+    })
+    s.addEventListener('click', function() {
+       game('s');
+    })
+
+}
+makeGame()
